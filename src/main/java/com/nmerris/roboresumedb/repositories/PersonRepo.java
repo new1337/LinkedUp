ba52884e7ec9d5b22898ef375d17fe8d5064fc58
nmerris <nate.merris@gmail.com>
2017-09-06 16:47:43 -0400

    removed all (hopefully) course stuff from templates.. note that summary page still exists and has refs, will need to update this later

--- src/main/java/com/nmerris/roboresumedb/repositories/PersonRepo.java
+++ src/main/java/com/nmerris/roboresumedb/repositories/PersonRepo.java
@@ -1,12 +1,8 @@
 package com.nmerris.roboresumedb.repositories;
 
-import com.nmerris.roboresumedb.models.Course;
 import com.nmerris.roboresumedb.models.Person;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PersonRepo extends CrudRepository<Person, Long> {
-
-    Iterable<Person> findAllByCoursesIs(Course someCourse);
-    Iterable<Person> findAllByIdIs(long id);
 
 }
