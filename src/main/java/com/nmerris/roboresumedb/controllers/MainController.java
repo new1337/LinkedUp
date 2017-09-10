b99da0535d231d2e27331a6b1c49435e6c0f1a9a
Nathan Merris <joorge.jetson@gmail.com>
2017-09-10 07:05:49 -0400

    addjob confirmation now shows each skill rating

--- src/main/java/com/nmerris/roboresumedb/controllers/MainController.java
+++ src/main/java/com/nmerris/roboresumedb/controllers/MainController.java
@@ -207,9 +207,6 @@ public class MainController {
             skillNames.add(skill.getSkill());
         }
 
-
-
-
         model.addAttribute("skillNames", skillNames);
 
         return "addjob";
@@ -744,6 +741,15 @@ public class MainController {
                 model.addAttribute("highLightPostList", false);
                 model.addAttribute("highLightSearch", false);
                 model.addAttribute("showDelete", true);
+
+                // make a Set of skill names, no duplicates in a set, user can pick from these, and also pick a rating
+                Set<String> skillNames = new LinkedHashSet<>();
+                skillNames.add("None Selected");
+                for (Skill skill : skillRepo.findAllByOrderBySkillAsc()) {
+                    skillNames.add(skill.getSkill());
+                }
+                model.addAttribute("skillNames", skillNames);
+
                 return "addjob";
         }
 
